c7b53c9d0e4efd93d6a3213f6946557a441a3f71
SRSteinkamp <simon.steinkamp@googlemail.com>
2020-04-03 12:15:10 +0200

    correlation_plots simon added

---
+++ plotCorrelations_simon.py
@@ -0,0 +1,86 @@
+from netCDF4 import Dataset
+import cartopy.crs as ccrs
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import pearsonr
+from sklearn.decomposition import PCA, FastICA
+import pandas as pd
+
+OUT_PATH = 'correlation_plots/'
+
+VUL =  Dataset('data/Volc_Forc_AOD_1st_mill.nc', 'r')['AOD'][:, 0, 0]
+SUN =  Dataset('data/Solar_forcing_1st_mill.nc', 'r')['TSI'][:, 0, 0]
+
+# unpacking variables
+R1 =  Dataset('data/T2m_R1_ym_1stMill.nc', 'r')
+temperature = R1.variables['T2m'][:]
+lat = R1.variables['lat'][:]
+lon = R1.variables['lon'][:]
+
+
+correlation_mat = np.zeros( (lat.shape[0], lon.shape[0], 2))
+for ii in range(lat.shape[0]):
+    for jj in range(lon.shape[0]):
+        correlation_mat[ii, jj, 0] = pearsonr(temperature[:, ii, jj], SUN)[0]
+        correlation_mat[ii, jj, 1] = pearsonr(temperature[:, ii, jj], VUL)[0]
+    
+plt.figure(figsize=(15, 5))
+plt.subplot(121)
+sns.heatmap(correlation_mat[:, :, 0], cmap='coolwarm', center=0)
+plt.title('Correlation with Sun')
+plt.subplot(122)
+sns.heatmap(correlation_mat[:, :, 1], cmap='coolwarm', center=0)
+plt.title('Correlation with Vol')
+plt.savefig(f'{OUT_PATH}/pearson_correlation_whole_map.png')
+
+
+NT_tropic_inx = np.where((lat <= 30) & (lat >= 0))[0]
+NT_middle_inx = np.where((lat <= 60) & (lat >= 30))[0]
+NT_polar_inx = np.where((lat <= 90) & (lat >= 60))[0]
+
+ST_tropic_inx = np.where((lat >= -30) & (lat <= 0))[0]
+ST_middle_inx = np.where((lat >= -60) & (lat <= -30))[0]
+ST_polar_inx = np.where((lat >= -90) & (lat <= -60))[0]
+
+# Indices
+zones = [ST_polar_inx, ST_middle_inx, ST_tropic_inx, NT_tropic_inx, NT_middle_inx, NT_polar_inx][::-1]
+
+
+lat_sep = 16
+zone_array = np.zeros((999, len(zones), lat_sep))
+labels = np.empty((len(zones), lat_sep), dtype='<U5')
+z_name = ['N_pol', 'N_mid', 'N_tr', 'S_tr', 'S_mid', 'S_pol']
+
+for jj in range(lat_sep):
+    for n, ii in enumerate(zones):
+        zone_array[:, n, jj] = np.mean(np.mean(temperature[:, ii, 12 * jj : 12 * (jj + 1)], 1), 1)
+        labels[n, jj] = f'{z_name[n]}_{jj}' #{np.mean(lon[12 * jj : 12 * (jj + 1)])}'
+
+zone_array_flat = np.reshape(zone_array, [-1, 16 * 6])
+
+
+corr_array_vul = np.zeros((16 * 6, 30))
+corr_array_sun = corr_array_vul.copy()
+
+for kk in range(30):
+    for n, ii in enumerate(zone_array_flat.T):
+        corr_array_vul[n, kk] = pearsonr(np.roll(ii, -kk), VUL)[0]
+        corr_array_sun[n, kk] = pearsonr(np.roll(ii, -kk), SUN)[0]
+
+corr_array_vul = pd.DataFrame(corr_array_vul.T, columns=labels.reshape(-1))
+corr_array_sun = pd.DataFrame(corr_array_sun.T, columns=labels.reshape(-1))
+
+
+plt.figure(figsize=(15,5))
+sns.heatmap(corr_array_vul, center=0, xticklabels=3)
+plt.ylabel('Years + x')
+plt.title('Correlations Volcanic')
+plt.savefig(f'{OUT_PATH}/correlations_vul.png', transparent=True, bbox_inches='tight')
+
+
+plt.figure(figsize=(15,5))
+sns.heatmap(corr_array_sun, center=0, xticklabels=3)
+plt.ylabel('Years + x')
+plt.title('Correlations Sun')
+plt.savefig(f'{OUT_PATH}/correlations_sun.png')
---
+++ sensor_helper.py
@@ -0,0 +1,92 @@
+from netCDF4 import Dataset
+import cartopy.crs as ccrs
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import pearsonr
+from sklearn.decomposition import PCA, FastICA
+import pandas as pd
+import geopandas as gpd
+import cartopy.io.shapereader as shpreader
+from tqdm.notebook import tqdm
+from sklearn.base import clone
+
+
+def plot_maps(temperature, lon, lat):
+    # Plots two subplots side by side
+    fig, axes = plt.subplots(1, len(temperature), figsize=(15, 5), subplot_kw={'projection': ccrs.PlateCarree()})
+    for kk in range(len(temperature)):
+        axes[kk].set_global()
+        axes[kk].coastlines()
+        axes[kk].contourf(lon, lat, temperature[kk],transform=ccrs.PlateCarree(), cmap='coolwarm') 
+    return fig, axes
+
+
+def load_simulation(set='R1'):
+    '''
+    Loads one of the simulation data sets
+    '''
+    R1 =  Dataset(f'data/T2m_{set}_ym_1stMill.nc', 'r')
+    temperature = R1.variables['T2m'][:]
+    lat = R1.variables['lat'][:]
+    lon = R1.variables['lon'][:]
+
+    return temperature, lat, lon
+
+
+def sensor_fields(nrows, ncols, divisor_row=6, divisor_col=6):
+    '''
+    Creates a grid of size nrows, ncols and sperates it into fields
+    by dividing nrows by divisior_row, etc.
+    '''
+    lan_division = np.int(ncols / divisor_col)
+    lon_division = np.int(nrows / divisor_row)
+
+    sensor_grid = np.zeros((nrows, ncols))
+    c = 0
+    for ii in range(divisor_col):
+        for jj in range(divisor_row):
+            sensor_grid[jj * lon_division : (jj+1) * lon_division, 
+                        ii * lan_division : (ii+1) * lan_division] = c
+            c += 1
+    return sensor_grid
+
+
+def fit_on_grid(X, y, clf, grid):
+    '''
+    Train multiple classifiers based on the grid information
+    '''
+    clf_list = []
+    for gg in np.unique(grid):
+        # Create a new classifier each round.
+        clf_list.append(clone(clf))
+        clf_list[-1].fit(X[:, grid==gg], y)
+    
+    return clf_list
+
+
+def predict_on_grid(X, y, clf_list, grid, proba=True):
+    '''
+    Predicts using multiple classifier
+    '''
+    # Instatniate
+    predictions = np.zeros((y.shape[0], np.unique(grid).shape[0]))
+    
+    # Loop over grid
+    for clf, gg in zip(clf_list, np.unique(grid)):
+        if proba:
+            predictions[:, gg.astype(int)] = clf.predict_proba(X[:, grid==gg])[:, 1]
+        else:
+            predictions[:, gg.astype(int)] = clf.predict(X[:, grid==gg])
+    return predictions
+
+
+def score_on_grid(y, y_pred, metric):
+    '''
+    Calculates a score for each prediction of a grid classifier.
+    '''
+    scores = np.zeros(y_pred.shape[1])
+    for ii in range(y_pred.shape[1]):
+        scores[ii] = metric(y, y_pred[:, ii])
+        
+    return scores
